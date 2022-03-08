9b13f3423ce02039278ea02cdcf2ee0d4d8435c7
PEDDALACHUGARI Rajesh (1) <rajesh.peddalachugari@hexagonsi.com>
2022-03-08 19:58:13 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import HelloWorld from "./components/Dashboard.vue";
+  export default {
+    components: {
+      HelloWorld: HelloWorld,
+    },
+  };
+</script>
+
+<template>
+  <HelloWorld></HelloWorld>
+</template>
+
+<style>
+  /* #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  } */
+</style>
